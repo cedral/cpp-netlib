4b4bdad2f1b7238ce238cf742565a57c992ef015
Glyn Matthews <glyn.matthews@gmail.com>
2012-08-06 20:24:03 +0200

    Re-added the one_liner example to the CMakeLists.txt script; Added all HTTP server code to CMakeLists.txt and uncommented code that should work.

--- libs/network/example/http/hello_world_server.cpp
+++ libs/network/example/http/hello_world_server.cpp
@@ -29,11 +29,11 @@ struct hello_world {
     /*<< This is the function that handles the incoming request. >>*/
     void operator() (server::request const &request,
                      server::response &response) {
-        //server::string_type ip = source(request);
-        //std::ostringstream data;
-        //data << "Hello, " << ip << "!";
-        //response = server::response::stock_reply(
-        //    server::response::ok, data.str());
+        auto ip = request.get_source();
+        std::ostringstream data;
+        data << "Hello, " << ip << "!";
+        response = server::response::stock_reply(
+            server::response::ok, data.str());
     }
     /*<< It's necessary to define a log function, but it's ignored in
          this example. >>*/
